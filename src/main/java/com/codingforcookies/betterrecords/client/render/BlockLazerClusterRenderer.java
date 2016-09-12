cde4426bc4e4c90cd433d214303a393b17d879d3
Nicholas <nickcfeldman@gmail.com>
2016-09-12 13:10:55 -0400

    Add model for Lazer Cluster
    Model used from 8c05ca2 of @Borlea's branch
    
    Closes #10

--- src/main/java/com/codingforcookies/betterrecords/client/render/BlockLazerClusterRenderer.java
+++ src/main/java/com/codingforcookies/betterrecords/client/render/BlockLazerClusterRenderer.java
@@ -1,24 +1,63 @@
 package com.codingforcookies.betterrecords.client.render;
 
 import com.codingforcookies.betterrecords.client.core.ClientProxy;
+import com.codingforcookies.betterrecords.client.model.ModelLazerCluster;
+import com.codingforcookies.betterrecords.common.BetterRecords;
 import com.codingforcookies.betterrecords.common.block.tile.TileEntityLazerCluster;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 public class BlockLazerClusterRenderer extends TileEntitySpecialRenderer<TileEntityLazerCluster> {
 
+    private static final ModelLazerCluster MODEL = new ModelLazerCluster();
+    private static final ResourceLocation TEXTURE = new ResourceLocation(BetterRecords.ID, "textures/models/lazercluster.png");
+
     @Override
-    public void renderTileEntityAt(TileEntityLazerCluster te, double x, double y, double z, float scale, int destroyStage) {
-        if(te == null)
+    public void renderTileEntityAt(TileEntityLazerCluster te, double x, double y, double z, float scale, int unknown) {
+        if (te == null) {
+            GL11.glPushMatrix();
+            {
+                GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+                GL11.glRotatef(180F, 180F, 0.0F, 1.0F);
+                GL11.glRotatef(180F, 0.0F, 1.0F, 0.0F);
+
+                bindTexture(TEXTURE);
+                MODEL.render(null, 0, 0, 0, 0.0F, 0.0F, 0.0625F);
+
+                GL11.glRotatef(-180F, -180F, 0.0F, 1.0F);
+                GL11.glTranslatef(0.0F, -.926F, 0.0F);
+            }
+            GL11.glPopMatrix();
             return;
+        }
 
         GL11.glPushMatrix();
         {
-            GL11.glTranslatef((float)x + 0.5F, (float)y + 0.5F, (float)z + 0.5F);
+            GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
             GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
 
-            // DRAW MODEL
+            bindTexture(TEXTURE);
+
+            MODEL.render(null, 0F, 0F, 0F, 0.0F, 0.0F, 0.0625F);
+
+            if(te.r != 0.0F && te.g != 0.0F && te.b != 0.0F) {
+                GL11.glDisable(GL11.GL_TEXTURE_2D);
+                GL11.glEnable(GL11.GL_BLEND);
+                GL11.glColor4f(te.r, te.g, te.b, (ClientProxy.flashyMode == 1 ? .2F : .4F));
+            }
+
+            MODEL.renderEmitor(null, 0F, 0F, 0F, 0.0F, 0.0F, 0.0625F);
+            if(te.r != 0.0F && te.g != 0.0F && te.b != 0.0F) {
+                GL11.glDisable(GL11.GL_BLEND);
+                GL11.glEnable(GL11.GL_TEXTURE_2D);
+            }
+
+            GL11.glColor4f(1F, 1F, 1F, 1F);
+            GL11.glEnable(GL11.GL_LIGHTING);
+
+            GL11.glTranslatef(0.0F, 1.0F, 0.0F);
 
             if(te.bass != 0 && ClientProxy.flashyMode > 0) {
                 GL11.glPushMatrix();
