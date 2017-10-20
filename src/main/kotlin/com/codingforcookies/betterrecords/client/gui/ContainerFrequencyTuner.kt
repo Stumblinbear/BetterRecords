8c9cbb3a1470698d36dff9368ad6763e68306957
Nicholas Feldman <nickcfeldman@gmail.com>
2017-10-19 23:12:58 -0400

    Convert ContainerFrequencyTuner and ContainerRecordEtcher to kotlin

---
+++ src/main/kotlin/com/codingforcookies/betterrecords/client/gui/ContainerFrequencyTuner.kt
@@ -0,0 +1,31 @@
+package com.codingforcookies.betterrecords.client.gui
+
+import com.codingforcookies.betterrecords.block.tile.TileFrequencyTuner
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.entity.player.InventoryPlayer
+import net.minecraft.inventory.Container
+import net.minecraft.inventory.Slot
+import net.minecraft.item.ItemStack
+
+class ContainerFrequencyTuner(inventoryPlayer: InventoryPlayer, var tileEntity: TileFrequencyTuner) : Container() {
+
+    init {
+        addSlotToContainer(SlotValid(tileEntity, 0, 17, 50))
+
+        for (i in 0..2) {
+            for (j in 0..8) {
+                addSlotToContainer(Slot(inventoryPlayer, j + i * 9 + 9, 8 + j * 18, 84 + i * 18))
+            }
+        }
+
+        for (i in 0..8) {
+            addSlotToContainer(Slot(inventoryPlayer, i, 8 + i * 18, 142))
+        }
+    }
+
+    override fun canInteractWith(player: EntityPlayer) =
+            tileEntity.isUsableByPlayer(player)
+
+    override fun transferStackInSlot(player: EntityPlayer?, slot: Int) =
+            null
+}
