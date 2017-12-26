75bd55244cca35571335c5bd67f7cc8443b50959
Nicholas <nickcfeldman@gmail.com>
2017-12-26 17:47:44 -0500

    Remove extra method in old PacketHandler
    Keeping myself organized

--- src/main/java/com/codingforcookies/betterrecords/common/packets/PacketHandler.java
+++ src/main/java/com/codingforcookies/betterrecords/common/packets/PacketHandler.java
@@ -1,7 +1,6 @@
 package com.codingforcookies.betterrecords.common.packets;
 
 import com.codingforcookies.betterrecords.api.connection.RecordConnection;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.network.FMLEmbeddedChannel;
 import net.minecraftforge.fml.common.network.FMLOutboundHandler;
 import net.minecraftforge.fml.relauncher.Side;
@@ -25,11 +24,6 @@ public class PacketHandler{
     public static void sendURLWriteFromClient(int x, int y, int z, String name, String url, String localName, int size){
         channels.get(Side.CLIENT).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.TOSERVER);
         channels.get(Side.CLIENT).writeOutbound(new PacketURLWriter(x, y, z, name, url, localName, size));
-    }
-
-    public static void sendRecordPlayToAllFromServer(int x, int y, int z, int dimension, float playRadius, NBTTagCompound nbt, boolean repeat, boolean shuffle){
-        channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.ALL);
-        //channels.get(Side.SERVER).writeOutbound(new PacketRecordPlayerPlay(x, y, z, playRadius, dimension, nbt, repeat, shuffle));
     }
 
 }
