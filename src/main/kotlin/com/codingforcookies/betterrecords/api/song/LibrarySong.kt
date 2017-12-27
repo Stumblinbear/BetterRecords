0dfb6bdbbe6856b9ff32e1cecf9d4c483c4af1f5
Nicholas <nickcfeldman@gmail.com>
2017-12-27 18:35:00 -0500

    Translate LibrarySong to Kotlin

---
+++ src/main/kotlin/com/codingforcookies/betterrecords/api/song/LibrarySong.kt
@@ -0,0 +1,9 @@
+package com.codingforcookies.betterrecords.api.song
+
+data class LibrarySong(
+        val local: String,
+        val author: String,
+        val name: String,
+        val url: String,
+        val color: Int
+)
