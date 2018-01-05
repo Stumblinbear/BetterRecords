9c3b3fe786a6af0c40e30fe7edd2218eb03f609f
Nicholas <nickcfeldman@gmail.com>
2018-01-04 23:46:24 -0500

    Update config when it has been changed

--- src/main/java/com/codingforcookies/betterrecords/ModConfig.java
+++ src/main/java/com/codingforcookies/betterrecords/ModConfig.java
@@ -1,6 +1,10 @@
 package com.codingforcookies.betterrecords;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * Config file for BetterRecords
@@ -43,5 +47,25 @@ public class ModConfig {
         @Config.Comment("Intensity of lights")
         @Config.RangeInt(min = -1, max = 3)
         public int flashMode;
+    }
+
+    public static void update() {
+        ConfigManager.sync(ConstantsKt.ID, Config.Type.INSTANCE);
+    }
+
+    @Mod.EventBusSubscriber
+    private static class EventHandler {
+
+        /**
+         * Inject the new values and save to the config file when the config has been changed from the GUI.
+         *
+         * @param event The event
+         */
+        @SubscribeEvent
+        public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+            if (event.getModID().equals(ConstantsKt.ID)) {
+                ModConfig.update();
+            }
+        }
     }
 }
